f1fd32ab8620e6524e041b0d819b8f03331c410d
Danil Dukhovnyi <duhovny.daniil@gmail.com>
2023-02-26 18:19:39 +0200

    Add Basket service

---
+++ src/Basket/OnlineShop.Basket.Api/BasketItem.cs
@@ -0,0 +1,12 @@
+﻿namespace OnlineShop.Domain;
+
+public class BasketItem
+{
+    public int Id { get; set; }
+
+    public string? Title { get; set; }
+
+    public decimal Price { get; set; }
+
+    public int ItemCount { get; set; }
+}
---
+++ src/Basket/OnlineShop.Basket.Api/Program.cs
@@ -0,0 +1,45 @@
+using OnlineShop.Domain;
+
+namespace OnlineShop.Basket.Api;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddAuthorization();
+
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapGet("api/items", (HttpContext httpContext) =>
+        {
+            var items = new List<BasketItem>
+            {
+                new BasketItem { Id = 1, Title = "Mivina", ItemCount = 2, Price = 14.05m },
+                new BasketItem { Id = 2, Title = "Redbull", ItemCount = 5, Price = 40.99m },
+                new BasketItem { Id = 3, Title = "Monster", ItemCount = 100, Price = 50.59m }
+            };
+
+            return items;
+        });
+
+        app.Run();
+    }
+}
