846ca3b2b0c704dd749c688e42540e9cbcd3cf1a
Ivanka <ivanka.polyuhovych@gmail.com>
2023-02-28 14:10:14 +0200

    Add Order service

---
+++ src/Orders/OnlineShop.Orders/OnlineShop.Orders.Api/Controllers/OrderController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShop.Orders.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        [HttpGet("/completedorders")]
+        public IEnumerable<Order> GetOrders(int userId)
+        {
+            var items = new List<Order>
+            {
+                new Order { Id = 1, UserId = 1, OrderDate = new DateTime(), BasketItems = new List<int> { 1, 1, 2 }, TotalPrice = 69.09m },
+                new Order { Id = 2, UserId = 1, OrderDate = new DateTime(), BasketItems = new List<int> { 2, 3 }, TotalPrice = 91.58m },
+                new Order { Id = 3, UserId = 2, OrderDate = new DateTime(), BasketItems = new List<int> { 1, 2 }, TotalPrice = 55 }
+            };
+
+            var orders = items.Where(o => o.UserId == userId);
+
+            return orders;
+        }
+
+        [HttpPost]
+        public Order GetOrders([FromQuery] int userId, [FromBody] List<int> basketItems)
+        {
+            var order = new Order
+            {
+                Id = 4,
+                UserId = userId,
+                OrderDate = new DateTime(),
+                BasketItems = basketItems,
+                TotalPrice = 34
+            };
+
+            return order;
+        }
+    }
+}
