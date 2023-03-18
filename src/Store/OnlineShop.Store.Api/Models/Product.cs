2f3c6a33e0ca718f4989c4143a94454be09d749f
Danil Dukhovnyi <duhovny.daniil@gmail.com>
2023-03-18 13:09:40 +0200

    Update Store entity

--- src/Store/OnlineShop.Store.Api/Models/Product.cs
+++ src/Store/OnlineShop.Store.Api/Models/Product.cs
@@ -3,11 +3,14 @@ namespace OnlineShop.Store.Api.Models
 {
     public class Product
     {
-        public int ProductId { get; set; }
-        public string Code { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public double Price { get; set; }
+        public int Id { get; set; }
 
+        public string Code { get; set; }
+        
+        public string Name { get; set; }
+        
+        public string Description { get; set; }
+        
+        public double Price { get; set; }
     }
 }
