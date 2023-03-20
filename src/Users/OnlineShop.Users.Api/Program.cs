86c8281acca6c812853de3f6cd164325881a8b12
Ivan Diordiev <i.diordev@gmail.com>
2023-03-20 18:11:56 +0200

    fix db deployments

--- src/Users/OnlineShop.Users.Api/Program.cs
+++ src/Users/OnlineShop.Users.Api/Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using OnlineShop.Users.Api.EF;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<UsersDbContext>(options =>
+{
+    options.UseNpgsql("");
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
