55bbc028c6715aea7a1d3b2a25bd15fd19056532
Ivan Diordiev <i.diordev@gmail.com>
2023-05-14 18:57:22 +0300

    Add http client

--- src/Users/OnlineShop.Users.Api/Program.cs
+++ src/Users/OnlineShop.Users.Api/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using OnlineShop.Users.Api.Controllers;
 using OnlineShop.Users.Api.EF;
 using OnlineShop.Users.Api.Options;
 
@@ -11,6 +12,8 @@ builder.Services.AddDbContext<UsersDbContext>(options =>
 	options.UseNpgsql(configuration.GetConnectionString("PostgresConnection")));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpClient<UsersController>();
 builder.Services.AddOptions<ServiceUrls>()
 	.Bind(configuration.GetSection(ServiceUrls.SectionName));
 
