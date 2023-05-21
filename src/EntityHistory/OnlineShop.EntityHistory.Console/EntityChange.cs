b16874909d32121b37d5496144767233b22a40ee
Ivan Diordiev <i.diordev@gmail.com>
2023-05-21 15:25:47 +0300

    Add entities for entity history

---
+++ src/EntityHistory/OnlineShop.EntityHistory.Console/EntityChange.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineShop.EntityHistory.Console;
+
+public class EntityChange
+{
+    public string EntityName { get; set; }
+    public int EntityId { get; set; }
+    public EntityChangeType ChangeType { get; set; }
+    public string? NewValue { get; set; }
+}
---
+++ src/EntityHistory/OnlineShop.EntityHistory.Console/EntityHistoryDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OnlineShop.EntityHistory.Console;
+
+public class EntityHistoryDbContext : DbContext
+{
+    
+    public DbSet<EntityChange> EntityChanges { get; set; }
+}
