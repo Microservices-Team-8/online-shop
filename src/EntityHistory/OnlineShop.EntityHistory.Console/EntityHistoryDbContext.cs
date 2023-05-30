05ee0f4f267e033b53796bd2f084b6c6c37b8cfe
Ivan Diordiev <i.diordev@gmail.com>
2023-05-30 10:50:27 +0300

    Revert "Add migration consoles"
    This reverts commit cf8f78e1

--- src/EntityHistory/OnlineShop.EntityHistory.Console/EntityHistoryDbContext.cs
+++ src/EntityHistory/OnlineShop.EntityHistory.Console/EntityHistoryDbContext.cs
@@ -4,10 +4,19 @@ using Microsoft.Extensions.Configuration;
 namespace OnlineShop.EntityHistory.Console;
 
 public class EntityHistoryDbContext : DbContext
-{ 
+{
+    private readonly IConfiguration _configuration;
+    
     public DbSet<EntityChangedMessage> EntityChanges { get; set; }
 
-    public EntityHistoryDbContext(DbContextOptions<EntityHistoryDbContext> options) : base(options)
+    public EntityHistoryDbContext(IConfiguration configuration)
     {
+        _configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseNpgsql(_configuration.GetConnectionString("PostgresConnection"));
     }
 }
